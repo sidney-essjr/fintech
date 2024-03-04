34db201ebcfa3be72f8a0907f7fd45df080f99eb
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-03-04 18:06:47 -0300

    feat: add the pages folder
    This folder contains the Summary component, which will be used to display the application's graphics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./Style.css";
+import Header from "./components/Header";
+import SideNav from "./components/SideNav";
+import Summary from "./pages/Summary";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <SideNav />
+      <main>
+        <Header />
+        <Summary />
+      </main>
+    </>
+  );
 }
 
 export default App;
