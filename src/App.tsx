8ea017ef59a0c1e22d74956702739e9be3254fc2
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-03-05 14:22:43 -0300

    :recicle: refactor: Adds handling of new states
    Moves states previously manipulated in DataRange to the DataContext
    component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
+import { Fragment } from "react/jsx-runtime";
 import "./Style.css";
+import DataRange from "./components/DataRange";
 import Header from "./components/Header";
 import SideNav from "./components/SideNav";
 import Summary from "./pages/Summary";
+import { DataContextProvider } from "./context/DataContext";
 
 function App() {
   return (
-    <>
-      <SideNav />
-      <main>
-        <Header />
-        <Summary />
-      </main>
-    </>
+    <div className="container">
+      <DataContextProvider>
+        <SideNav />
+        <main>
+          <Header />
+          <DataRange />
+          <Summary />
+        </main>
+      </DataContextProvider>
+    </div>
   );
 }
 
--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,4 +1,10 @@
-import { Dispatch, PropsWithChildren, createContext, useState } from "react";
+import {
+  Dispatch,
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useState,
+} from "react";
 import useFetch, { FetchState } from "../hooks/useFetch";
 
 type Data = {
@@ -12,18 +18,39 @@ type Data = {
 };
 
 type IFetchData = FetchState<Data> & {
-  setUrl: Dispatch<React.SetStateAction<string>>;
+  start: string;
+  setStart: Dispatch<React.SetStateAction<string>>;
+  end: string;
+  setEnd: Dispatch<React.SetStateAction<string>>;
 };
 
 const DataContext = createContext<IFetchData | null>(null);
 
-const DataContextProvider = ({ children }: PropsWithChildren) => {
-  const [url, setUrl] = useState("https://data.origamid.dev/vendas/");
+export const useData = () => {
+  const context = useContext(DataContext);
+  if (!context) throw new Error("The context must involve your dependents");
+  return context;
+};
+
+function getInitialDate(date: Date = new Date(), daysAgo: number) {
+  date.setDate(date.getDate() - daysAgo);
+  const dd = String(date.getDate()).padStart(2, "0");
+  const mm = String(date.getMonth() + 1).padStart(2, "0");
+  const yyyy = date.getFullYear();
+  return `${yyyy}-${mm}-${dd}`;
+}
+
+export const DataContextProvider = ({ children }: PropsWithChildren) => {
+  const [start, setStart] = useState(getInitialDate(undefined, 7));
+  const [end, setEnd] = useState(getInitialDate(undefined, 0));
+  const url = `https://data.origamid.dev/vendas/?inicio=${start}final=${end}`;
 
   const { data, loading, error } = useFetch<Data>(url);
 
   const context = (
-    <DataContext.Provider value={{ data, error, loading, setUrl }}>
+    <DataContext.Provider
+      value={{ data, error, loading, start, setStart, end, setEnd }}
+    >
       {children}
     </DataContext.Provider>
   );
