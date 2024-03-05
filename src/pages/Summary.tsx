050d3d450c36103afc2ee0751d7bc148c6bfdcaa
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-03-05 16:26:06 -0300

    feat: Implements the dashboards in the summary
    Dashboards are implemented with the sum of total sales exceeding those that resulted in failure, the sums of consolidated sales and sales still in processing are also displayed

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -1,3 +1,40 @@
+import { useData } from "../context/DataContext";
+
 export default function Summary() {
-  return <div>Summary</div>;
+  const { data } = useData();
+  if (data === null) return null;
+  return (
+    <section>
+      <div className="resumo flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((i) => i.status !== "falha")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((i) => i.status === "pago")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((i) => i.status === "processando")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+      </div>
+      <div className="box">Gráficos</div>
+    </section>
+  );
 }
