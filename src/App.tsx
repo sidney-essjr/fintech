697cfcfde3c521c083789a5782ae1df1649100c6
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-03-08 16:25:05 -0300

    feat: implements routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./Style.css";
 import Header from "./components/Header";
 import SideNav from "./components/SideNav";
 import { DataContextProvider } from "./context/DataContext";
 import Sales from "./pages/Sales";
 import Summary from "./pages/Summary";
+import Sale from "./pages/Sale";
 
 function App() {
   return (
-    <div className="container">
+    <BrowserRouter>
       <DataContextProvider>
-        <SideNav />
-        <main>
-          <Header />
-          <Summary />
-          <Sales />
-        </main>
+        <div className="container">
+          <SideNav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Summary />} />
+              <Route path="/vendas" element={<Sales />} />
+              <Route path="/vendas/:id" element={<Sale />} />
+            </Routes>
+          </main>
+        </div>
       </DataContextProvider>
-    </div>
+    </BrowserRouter>
   );
 }
 
